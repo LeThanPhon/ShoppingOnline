e02c7e0f51ff1ff3b47c193087b4c939e86750aa
LeThanPhon <127474649+LeThanPhon@users.noreply.github.com>
2024-07-16 19:56:09 +0700

    Add files via upload

---
+++ client-admin/src/components/LoginComponent.js
@@ -0,0 +1,79 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+import MyContext from '../contexts/MyContext';
+import '../bootstrap/css/bootstrap.min.css'
+import '../css/ionicons.min.css'
+import '../css/style.css'
+
+class Login extends Component {
+  static contextType = MyContext; // using this.context to access global state
+  constructor(props) {
+    super(props);
+    this.state = {
+      txtUsername: '',
+      txtPassword: ''
+    };
+  }
+  render() {
+    if (this.context.token === '') {
+      return (
+        <div class="main_content">
+          <div class="login_register_wrap section">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6 col-md-10">
+                        <div class="login_wrap">
+                            <div class="padding_eight_all bg-white">
+                                <div class="heading_s1">
+                                    <h3>Admin Login</h3>
+                                </div>
+                                <form method="post">
+                                    <div class="form-group mb-3">
+                                      <input type="text"class="form-control" placeholder='Username' value={this.state.txtUsername} onChange={(e) => { this.setState({ txtUsername: e.target.value }) }} />
+                                    </div>
+                                    <div class="form-group mb-3">
+                                      <input type="password" class="form-control" placeholder='Password' value={this.state.txtPassword} onChange={(e) => { this.setState({ txtPassword: e.target.value }) }} />
+                                    </div>
+                                    <div class="form-group mb-3">
+                                        <input type="submit" class="btn btn-fill-out btn-block" value="Login" onClick={(e) => this.btnLoginClick(e)} />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return (<div />);
+  }
+  // event-handlers
+  btnLoginClick(e) {
+    e.preventDefault();
+    const username = this.state.txtUsername;
+    const password = this.state.txtPassword;
+    if (username && password) {
+      const account = { username: username, password: password };
+      this.apiLogin(account);
+    } else {
+      alert('Please input username and password');
+    }
+  }
+  // apis
+  apiLogin(account) {
+    axios.post('/api/admin/login', account).then((res) => {
+      const result = res.data;
+      if (result.success === true) {
+        this.context.setToken(result.token);
+        this.context.setUsername(account.username);
+      } else {
+        alert(result.message);
+      }
+    });
+  }
+}
+export default Login;
+
+
