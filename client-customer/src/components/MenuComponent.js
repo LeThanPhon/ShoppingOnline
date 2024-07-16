876f61064c7372c438380906c0cdc7a75eed7a89
LeThanPhon <127474649+LeThanPhon@users.noreply.github.com>
2024-07-17 00:36:39 +0700

    Update MenuComponent.js

--- client-customer/src/components/MenuComponent.js
+++ client-customer/src/components/MenuComponent.js
@@ -22,8 +22,9 @@ class Menu extends Component {
       <div className="border-bottom">
         <div className="float-left">
           <ul className="menu">
-          <li className="menu"><Link to='/'>Home</Link></li>
-            {cates}
+            <li className="menu"><Link to='/'>Home</Link></li>
+              {cates}
+            <li className="menu"><Link to='/gmap'>Gmap</Link></li>
           </ul>
         </div>
         <div className="float-right">
