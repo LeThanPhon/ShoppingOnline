c604e3d755fc192ec0a8affaa427d7743d72166d
LeThanPhon <127474649+LeThanPhon@users.noreply.github.com>
2024-07-17 20:32:08 +0700

    Add files via upload

---
+++ client-customer/src/components/TawkMessengerComponent.js
@@ -0,0 +1,11 @@
+import React, { Component }  from "react";
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
+
+class TaskMessengerReact extends Component {
+    render() {
+        return (
+            <TawkMessengerReact propertyId='66978efebecc2fed69269788' widgetId='1i300d63q' />
+        );
+    }
+}
+export default TawkMessenger;
